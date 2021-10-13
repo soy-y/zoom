0308cee834333c7ceb85050fe52f5ead7457c511
soy-y <55077961+soy-y@users.noreply.github.com>
2021-10-13 20:22:44 +0900

    before delete all

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import http from "http";
-import Websocket from "ws";
+// import Websocket from "ws";
+import SocketIO from "socket.io";
 import express from "express";
-import { parse } from "path";
 
 const app = express();
 
@@ -15,31 +15,32 @@ app.get("/*", (req, res) => res.redirect("/"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 //app.listen(3000, handleListen);
 
-const server = http.createServer(app);
+const httpServer = http.createServer(app);
 //이렇게 작성하면 http서버, websocket서버 둘 다 돌릴 수 있음.
 //2개의 protocol, 다 같은 port를 공유하는 것
-const wss = new Websocket.Server( {server} );
+// const wss = new Websocket.Server( {server} );
+const wsServer = SocketIO(httpServer);
 
-//fake database. 다른 브라우저들과도 연결해주기 위함
-const sockets = [];
-//여기서 socket은 연결된 브라우저
-wss.on("connection", (socket) =>{
-    sockets.push(socket);
-    socket["nickname"] = "Anon"
-    console.log("Connected to Browser ✔");
-    socket.on("close", () => console.log("Disconnected to Browser ❌"));
-    socket.on("message", (msg) => {
-        const message = JSON.parse(msg);
-        switch(message.type) {
-            case "new_message":
-                sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`));
-                break;
-            case "nickname":
-                console.log("nick:",message.payload);
-                socket["nickname"] = message.payload;
-                break;
-        }
-    });
-});
+// fake database. 다른 브라우저들과도 연결해주기 위함
+// const sockets = [];
+// //여기서 socket은 연결된 브라우저
+// wss.on("connection", (socket) =>{
+//     sockets.push(socket);
+//     socket["nickname"] = "Anon"
+//     console.log("Connected to Browser ✔");
+//     socket.on("close", () => console.log("Disconnected to Browser ❌"));
+//     socket.on("message", (msg) => {
+//         const message = JSON.parse(msg);
+//         switch(message.type) {
+//             case "new_message":
+//                 sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`));
+//                 break;
+//             case "nickname":
+//                 console.log("nick:",message.payload);
+//                 socket["nickname"] = message.payload;
+//                 break;
+//         }
+//     });
+// });
 
-server.listen(3000, handleListen);
+httpServer.listen(3000, handleListen);
